59fcd8fd34f5db1bc34278328e31de254dba6ee4
federicogiannini <federico.giannini.13@gmail.com>
2024-10-25 17:56:14 +0200

    configurations

---
+++ lab/dataset_utils.py
@@ -0,0 +1,125 @@
+import os
+import itertools
+import numpy as np
+import pandas as pd
+import pickle
+
+
+def gen_conf(set1, set2):
+    set2_perm = itertools.permutations(set2)
+    all_couples = [[(x, y) for x, y in zip(set1, perm)] for perm in set2_perm]
+    index = np.random.randint(0, len(all_couples))
+    sel_couples = all_couples[index]
+    np.random.shuffle(sel_couples)
+    return sel_couples
+
+
+def build_conf_df(root, dataset, df, experiences, conf, train="train"):
+    dfs_conf = []
+    for task, exp in enumerate(experiences):
+        df_exp = df[df["target"].isin(exp)].reset_index(drop=True)
+        df_exp["target"] = df_exp["task1"]
+        df_exp = df_exp.drop(columns=[f"task{i}" for i in range(1, 6)]).sample(frac=1).reset_index(drop=True)
+        df_exp = df_exp.iloc[:len(df_exp) // 10 * 10]
+        df_exp["task"] = task + 1
+        dfs_conf.append(df_exp)
+    df_conf = pd.concat(dfs_conf).reset_index(drop=True)
+    df_conf.to_csv(os.path.join(root, "datasets", f"{dataset}_incremental_{conf}conf_{train}.csv"),
+                   index=False)
+    return df_conf
+
+
+def make_exp_incremental_train(root, dataset, df, set1, set2, nconfs=10):
+    try:
+        with open(os.path.join(root, "datasets", f"{dataset}_incremental_confs.pkl"), "rb") as f:
+            confs = pickle.load(f)
+    except:
+        confs = []
+    count_confs = 0
+    dfs = []
+    while (count_confs < nconfs):
+        experiences = gen_conf(set1, set2)
+        print(experiences, end=" ")
+        if experiences in confs:
+            print("REJECTED")
+            continue
+        print("ACCEPTED")
+        confs.append(experiences)
+        count_confs += 1
+        df_conf = build_conf_df(root, dataset, df, experiences, len(confs), "train")
+        dfs.append(df_conf)
+    with open(os.path.join(root, "datasets", f"{dataset}_incremental_confs.pkl"), "wb") as f:
+        pickle.dump(confs, f)
+    return dfs
+
+
+def make_exp_incremental_test(root, dataset, df):
+    with open(os.path.join(root, "datasets", f"{dataset}_incremental_confs.pkl"), "rb") as f:
+        confs = pickle.load(f)
+    dfs = []
+    for count, experiences in enumerate(confs):
+        df_conf = build_conf_df(root, dataset, df, experiences, count + 1, "test")
+        dfs.append(df_conf)
+    return dfs
+
+def get_conf_perm():
+    perms = list(itertools.permutations([i for i in range(1, 6)]))
+    index = np.random.randint(0, len(perms))
+    return perms[index]
+
+
+def build_conf_df_sml(root, dataset, df, conf, conf_number, train="train"):
+    n_split = 5
+    dfs = [[] for _ in range(n_split)]
+    for target in df["target"].unique():
+        df_target = df[df["target"] == target].sample(frac=1)
+        len_split = len(df_target) // n_split
+        ranges = [(i * len_split, (i + 1) * len_split) for i in range(n_split - 1)] + [
+            ((n_split - 1) * len_split, len(df_target))]
+        for i, (start, end) in enumerate(ranges):
+            dfs[i].append(df_target.iloc[start:end, :].reset_index(drop=True))
+
+    final_dfs = [pd.concat(dfs_).sample(frac=1).reset_index(drop=True) for dfs_ in dfs]
+    for i, task in enumerate(conf):
+        final_dfs[i]["target"] = final_dfs[i][f"task{task}"]
+        final_dfs[i]["task"] = i + 1
+        final_dfs[i] = final_dfs[i].drop(columns=[f"task{j}" for j in range(1, 6)])
+        final_dfs[i] = final_dfs[i].iloc[:len(final_dfs[i]) // 10 * 10, :]
+    final_df = pd.concat(final_dfs).reset_index(drop=True)
+    final_df.to_csv(os.path.join(root, "datasets", f"{dataset}_sml_{conf_number}conf_{train}.csv"),
+                    index=False)
+    return final_df
+
+
+def make_exp_sml_train(root, dataset, df, nconfs=10):
+    try:
+        with open(os.path.join(root, "datasets", f"{dataset}_sml_confs.pkl"), "rb") as f:
+            confs = pickle.load(f)
+    except:
+        confs = []
+    count_conf = 0
+
+    final_dfs = []
+    while count_conf < nconfs:
+        conf = get_conf_perm()
+        print(conf, end=" ")
+        if conf in confs:
+            print("REJECTED")
+            continue
+        print("ACCEPTED")
+        confs.append(conf)
+        count_conf += 1
+        final_dfs.append(build_conf_df_sml(root, dataset, df, conf, len(confs), "train"))
+    with open(os.path.join(root, "datasets", f"{dataset}_sml_confs.pkl"), "wb") as f:
+        pickle.dump(confs, f)
+    return final_dfs
+
+
+def make_exp_sml_test(root, dataset, df):
+    with open(os.path.join(root, "datasets", f"{dataset}_sml_confs.pkl"), "rb") as f:
+        confs = pickle.load(f)
+    dfs = []
+    for count, experiences in enumerate(confs):
+        df_conf = build_conf_df_sml(root, dataset, df, experiences, count + 1, "test")
+        dfs.append(df_conf)
+    return dfs
--- run_sml.py
+++ run_sml.py
@@ -1,4 +1,7 @@
-DATASET = "fashion_mnist_red50_incremental"
+DATASETS = [
+    f"fashion_mnist_red50_incremental_{c}conf"
+    for c in range(1,6)
+]
 ROOT = "/Users/federicogiannini/Library/CloudStorage/OneDrive-PolitecnicodiMilano/SML_CL"
 
 import pickle
@@ -9,89 +12,90 @@ from utils.sml_utils import test_cl
 import os
 from utils.utils import return_metrics, make_dir
 
-df = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"), nrows=1)
-last_task = df["task"].iloc[0]
-converters = {c: float for c in df.columns if "feat" in c}
-converters["target"] = int
-converters["task"] = int
+for DATASET in DATASETS:
+    df = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"), nrows=1)
+    last_task = df["task"].iloc[0]
+    converters = {c: float for c in df.columns if "feat" in c}
+    converters["target"] = int
+    converters["task"] = int
 
-data_stream = stream.iter_csv(
-    os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"),
-    converters=converters,
-    target="target"
-)
-
-models = {
-    "arf": forest.ARFClassifier(leaf_prediction="nb"),
-    "hat": tree.HoeffdingAdaptiveTreeClassifier(
-        grace_period=100,
-        delta=1e-5,
-        leaf_prediction="nb",
-        nb_threshold=10,
+    data_stream = stream.iter_csv(
+        os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"),
+        converters=converters,
+        target="target"
     )
-}
 
-perf = {
-    m: {
-        "total": return_metrics(),
-        "concept": return_metrics()
-    } for m in models
-}
+    models = {
+        "arf": forest.ARFClassifier(leaf_prediction="nb"),
+        "hat": tree.HoeffdingAdaptiveTreeClassifier(
+            grace_period=100,
+            delta=1e-5,
+            leaf_prediction="nb",
+            nb_threshold=10,
+        )
+    }
 
-perf_values = {
-    m: {
-        "total": {"accuracy": [], "kappa": []},
-        "concept": {"accuracy": [], "kappa": []}
-    } for m in models
-}
-perf_values["drifts"] = []
+    perf = {
+        m: {
+            "total": return_metrics(),
+            "concept": return_metrics()
+        } for m in models
+    }
 
-predictions = {m:[] for m in models}
+    perf_values = {
+        m: {
+            "total": {"accuracy": [], "kappa": []},
+            "concept": {"accuracy": [], "kappa": []}
+        } for m in models
+    }
+    perf_values["drifts"] = []
 
-
-cl_table = {
-    m: {
-        metric: [] for metric in ["accuracy", "kappa"]
-    } for m in models
-}
-
-df_test = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_test.csv"))
-
-X_test = []
-y_test = []
-for task in df_test["task"].unique():
-    df_task = df_test[df_test["task"]==task]
-    X_test.append(df_task.iloc[:,:-2].values)
-    y_test.append(list(df_task["target"]))
+    predictions = {m:[] for m in models}
 
 
-df = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"))
+    cl_table = {
+        m: {
+            metric: [] for metric in ["accuracy", "kappa"]
+        } for m in models
+    }
+
+    df_test = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_test.csv"))
+
+    X_test = []
+    y_test = []
+    for task in df_test["task"].unique():
+        df_task = df_test[df_test["task"]==task]
+        X_test.append(df_task.iloc[:,:-2].values)
+        y_test.append(list(df_task["target"]))
 
 
-for idx, (x, y) in enumerate(data_stream):
-    print(f"{DATASET} Prequential {idx+1}", end="\r")
-    if last_task != x["task"]:
-        print()
-        print(f"DRIFT {idx+1}")
-        for m in perf:
-            perf[m]["concept"] = return_metrics()
-        cl_table = test_cl(cl_table, models, X_test, y_test)
-        perf_values["drifts"].append(idx)
-    last_task = x["task"]
-    del x["task"]
-    for m in models:
-        pred = models[m].predict_one(x)
-        pred = 0 if pred is None else pred
-        predictions[m].append(pred)
-        for method in ["total", "concept"]:
-            for metric in perf[m][method]:
-                perf[m][method][metric].update(y, pred)
-                perf_values[m][method][metric].append(perf[m][method][metric].get())
-        models[m].learn_one(x, y)
-cl_table = test_cl(cl_table, models, X_test, y_test)
+    df = pd.read_csv(os.path.join(ROOT, "datasets", f"{DATASET}_train.csv"))
 
-make_dir(os.path.join(ROOT, "performance", DATASET))
-with open(os.path.join(ROOT, "performance", DATASET, "performance_sml.pkl"), "wb") as f:
-    pickle.dump(perf_values, f)
-with open(os.path.join(ROOT, "performance", DATASET, "cl_table_sml.pkl"), "wb") as f:
-    pickle.dump(cl_table, f)
+
+    for idx, (x, y) in enumerate(data_stream):
+        print(f"{DATASET} Prequential {idx+1}", end="\r")
+        if last_task != x["task"]:
+            print()
+            print(f"DRIFT {idx+1}")
+            for m in perf:
+                perf[m]["concept"] = return_metrics()
+            cl_table = test_cl(cl_table, models, X_test, y_test)
+            perf_values["drifts"].append(idx)
+        last_task = x["task"]
+        del x["task"]
+        for m in models:
+            pred = models[m].predict_one(x)
+            pred = 0 if pred is None else pred
+            predictions[m].append(pred)
+            for method in ["total", "concept"]:
+                for metric in perf[m][method]:
+                    perf[m][method][metric].update(y, pred)
+                    perf_values[m][method][metric].append(perf[m][method][metric].get())
+            models[m].learn_one(x, y)
+    cl_table = test_cl(cl_table, models, X_test, y_test)
+
+    make_dir(os.path.join(ROOT, "performance", DATASET))
+    with open(os.path.join(ROOT, "performance", DATASET, "performance_sml.pkl"), "wb") as f:
+        pickle.dump(perf_values, f)
+    with open(os.path.join(ROOT, "performance", DATASET, "cl_table_sml.pkl"), "wb") as f:
+        pickle.dump(cl_table, f)
